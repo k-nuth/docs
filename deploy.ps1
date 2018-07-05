2c4e4e25c21daed1a85afc24e44e8284e498af39
Mario Dal Lago <the4power@yahoo.com.ar>
2018-07-05 15:07:43 -0300

    move deploy to script

---
+++ deploy.ps1
@@ -0,0 +1,10 @@
+git config --global user.email "ci@bitprim.org"
+git config --global user.name "Bitprim CI"
+git clone https://github.com/bitprim/bitprim.github.io.git
+cd _site
+xcopy *.* C:\projects\bitprim-docs\bitprim.github.io\docfx\ /s /e /y
+cd ../bitprim.github.io
+git add .
+git commit -m "Updated documentation site"
+git remote add origin-commit https://$($env:access_token)@github.com/bitprim/bitprim.github.io.git
+git push --quiet --set-upstream origin-commit master
