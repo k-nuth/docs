99a79dbe04d7a72d5ec5c1d13c4e16f377445464
dario-ramos <dario.fenix@gmail.com>
2018-07-10 15:30:03 -0300

    [BIT-177] Try setting Git credentials

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,7 @@
+git config --global credential.helper store
 git config --global user.email "ci@bitprim.org"
 git config --global user.name "Bitprim CI"
+Add-Content "$HOME\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"
 git clone https://github.com/bitprim/bitprim.github.io.git
 cd _site
 xcopy *.* C:\projects\bitprim-docs\bitprim.github.io\docfx\ /s /e /y
